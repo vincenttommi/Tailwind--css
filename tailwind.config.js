d879994d02c968c48c09fe1a1aaaaf86c8836252
vincenttommi <vincenttommi@gmail.com>
2023-05-04 21:20:36 +0300

    colors in tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+const browserslist = require('browserslist');
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
